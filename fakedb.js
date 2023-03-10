047c15ce7a6eac5181a617aa37c1c6f2815a576f
Aman goyal <amangoyal@Amans-MacBook-Air.local>
2023-03-10 13:21:43 +0530

    first commit

---
+++ fakedb.js
@@ -0,0 +1,63 @@
+const users = [
+    {
+      id: 1,
+      fname: "Aarav",
+      lname: "Patel",
+      email: "aarav.patel@example.com",
+      password: "mypassword123"
+    },
+    {
+      id: 2,
+      fname: "Aanya",
+      lname: "Shah",
+      email: "aanya.shah@example.com",
+      password: "mypassword456"
+    },
+    {
+      id: 3,
+      fname: "Aarush",
+      lname: "Singh",
+      email: "aarush.singh@example.com",
+      password: "mypassword789"
+    },
+    {
+      id: 4,
+      fname: "Diya",
+      lname: "Gupta",
+      email: "diya.gupta@example.com",
+      password: "mypasswordabc"
+    },
+    {
+      id: 5,
+      fname: "Kabir",
+      lname: "Joshi",
+      email: "kabir.joshi@example.com",
+      password: "mypassworddef"
+    }
+  ];
+const quotes = [
+    {
+      quote: "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+      by: 2
+    },
+    {
+      quote: "If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough.",
+      by: 1
+    },
+    {
+      quote: "The future belongs to those who believe in the beauty of their dreams.",
+      by: 1
+    },
+    {
+      quote: "Believe you can and you're halfway there.",
+      by: 2
+    },
+    {
+      quote: "What you get by achieving your goals is not as important as what you become by achieving your goals.",
+      by: 3
+    }
+  ];
+
+  module.exports={
+    users,quotes
+  }
